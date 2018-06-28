e8593e20488d68a684a57678edc440e1efd92f47
Maxwell Gover <maxwellrgover@gmail.com>
2018-06-27 22:02:18 -0400

    Allow user to add custom song name

--- src/components/Uploader/index.js
+++ src/components/Uploader/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 
 import { storage, db } from "../../initializers/firebase";
 
@@ -6,7 +6,11 @@ class Uploader extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { }
+    this.state = {
+      songUploaded: false,
+      songName: '',
+      downloadUrl: '',
+    }
   }
 
   handleUpload = (event) => {
@@ -18,22 +22,38 @@ class Uploader extends Component {
       .then((snapshot) => { 
         console.log(snapshot.metadata)
         snapshot.ref.getDownloadURL()
-          .then((downloadUrl) => { 
-            db.collection('users').doc(this.props.user.id).collection('uploadedSongs').add({
-              name: 'mySong',
-              url: downloadUrl,
-            })
-            .then(() => { console.log('Song added to the db successfully')})
-            .catch((error) => { console.warn(error)})
-          });
-        // Store url to user in db under uploadedSongs -> song: { title: '', url: ''}
+          .then((downloadUrl) => {
+            this.setState({ songUploaded: true, downloadUrl });
+          })
+          .catch((error) => { console.log(error) })
       })
       .catch(() => {})
   }
+
+  addSong = () => {
+    db.collection('users').doc(this.props.user.id).collection('uploadedSongs').add({
+      name: this.state.songName, // placeholder until user inputs a song name
+      url: this.state.downloadUrl,
+    })
+    .then(() => { console.log('Song added to the db successfully')})
+    .catch((error) => { console.warn(error)})
+  }
+
+  addSongName = (event) => {
+    this.setState({ songName: event.target.value })
+  }
+
   render() {
     return (
       <div>
         <input type="file" onChange={(event) => this.handleUpload(event)} />
+        {this.state.songUploaded && (
+          <Fragment>
+            <input onChange={(event) => this.addSongName(event)} />
+            <button onClick={() => this.addSong()}>Add song</button>
+          </Fragment>
+          )
+        }
       </div>
     );
   }
